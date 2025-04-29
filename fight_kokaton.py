8103803532925167c766b9c72f5b1a456d0018d5
c0c24056cb <c0c24056cb@edu.teu.ac.jp>
2025-04-29 15:55:48 +0900

    演習1完成

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -140,6 +140,22 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+class Score:
+    def __init__(self):
+        self.fonto = pg.font.SysFont("hgp創英角ポップ体", 30)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.img = self.fonto.render(f"Score: {self.score}", True, self.color)
+        self.rct = self.img.get_rect()
+        self.rct.center = (100, HEIGHT - 50)
+
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render(f"Score: {self.score}", True, self.color)
+        screen.blit(self.img, self.rct)
+
+    def add_score(self, amount: int = 1):
+        self.score += amount
+
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
@@ -149,6 +165,7 @@ def main():
     beam = None
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -179,6 +196,7 @@ def main():
                     beam = None  # ビームを消す
                     bombs[j] = None  # 爆弾を消す
                     bird.change_img(6, screen)  # よろこびエフェクト
+                    score.add_score(1)            
             bombs = [bomb for bomb in bombs if bomb is not None]  # 撃ち落とされてない爆弾だけのリストにする
 
         key_lst = pg.key.get_pressed()
@@ -187,10 +205,12 @@ def main():
             beam.update(screen)
         for bomb in bombs:
             bomb.update(screen)
+
+        score.update(screen)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
